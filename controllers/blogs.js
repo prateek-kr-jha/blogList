0c64343c8010b1b6494ffc0b12098497d59907f3
Prateek <prateekj1211@gmail.com>
2025-03-04 23:15:16 +0530

    Added default likes in post api in case likes are missing

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,7 +8,13 @@ blogRouter.get('/', async (request, response, next) => {
 })
 
 blogRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body);
+  const newBlog = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes ? request.body.likes : 0
+  }
+  const blog = new Blog(newBlog);
 
   const returnedBlog = await blog.save();
   return response.status(201).json(returnedBlog);

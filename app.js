304da273db0e76e444d2214673bbcd91e67d28aa
Prateek <prateekj1211@gmail.com>
2025-03-13 00:30:48 +0530

    added token based login

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const cors = require('cors');
 const middleware = require('./utils/middleware');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 
--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 const TEST_MONGODB_URI = process.env.TEST_MONGODB_URI;
+const SECRET = process.env.SECRET;
 
 module.exports = {
     PORT,
     MONGODB_URI,
-    TEST_MONGODB_URI
+    TEST_MONGODB_URI,
+    SECRET
 }

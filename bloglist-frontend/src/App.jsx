eed4a347d2e80fd69b77eddaf1239af1d78218a6
Prateek <prateekj1211@gmail.com>
2025-03-31 15:15:44 +0530

    Added view details functionality

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -133,8 +133,9 @@ const App = () => {
         <br />
         {BlogForm()}
         <br/>
+        
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+            <Blog key={blog.id} blog={blog} />
         )} 
 
     </div>
--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  console.log(blog)
+  const [showDetail, setShowDetail] = useState(false)
+  const showWhenHidden = {display: showDetail ? 'none': ''}
+  const showWhenVisible= {display: showDetail ? '': 'none'}
+  const toggleShowDetail = () => {
+    setShowDetail(!showDetail)
+  }
+  return (
+  <div className="blog-container">
+    <h4>
+      {blog.title}
+      <button style={showWhenHidden} onClick={toggleShowDetail}>View</button>
+      <button style={showWhenVisible} onClick={toggleShowDetail}>Hide</button>
+    </h4>
+    <div style={showWhenVisible}>
+      <h4>
+        {blog.url}
+      </h4>
+      <h4>
+        Likes {blog.likes}
+      </h4>
+      <h4>
+        {blog.author}
+      </h4>
+    </div>
+
+  </div> ) 
+}
 
 export default Blog
